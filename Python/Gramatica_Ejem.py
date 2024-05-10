d871a317ff9c2469a3c2333ddc7225303590bc7c
エンジニア <167157677+Lucc4z@users.noreply.github.com>
2024-05-10 11:38:49 -0500

    Gramatica_Ejem.py

--- Python/Gramatica_Ejem.py
+++ Python/Gramatica_Ejem.py
@@ -1,20 +1,16 @@
 import nltk
 
-# Definimos los símbolos no terminales
 v0 = nltk.Nonterminal('v0')
 v1 = nltk.Nonterminal('v1')
 a = nltk.Nonterminal('a')
 b = nltk.Nonterminal('b')
 
-# Definimos las reglas de producción
 production_rules = [
     nltk.Production(v0, ['a', v1]),
     nltk.Production(v1, ['b', v0]),
     nltk.Production(v1, ['a'])
 ]
 
-# Creamos la gramática
 grammar = nltk.CFG(v0, production_rules)
 
-# Imprimimos la gramática
 print(grammar)
